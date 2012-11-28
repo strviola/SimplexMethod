7de69510ca932802b8892498dfbbb35d3786a647
Dragon <suzuki.r.ak@m.titech.ac.jp>
2012-11-28 19:12:06 +0900

    Tabuleauモデルの改良

--- simplex/tabuleau.py
+++ simplex/tabuleau.py
@@ -8,10 +8,11 @@ class Variable:
     def __init__(self, val, slack, dom=None, dom_type=None):
         self.val = float(val)
         self.slack = bool(slack)
+        self.dom = dom
 
         # define the domain condition
         if dom is None and dom_type is None:
-            # no domain condition
+            # no domain condition (whole area)
             self.cond = lambda dummy_arg: True
         elif dom is not None and dom_type is not None:
             # there is domain condition
@@ -30,24 +31,40 @@ class Variable:
     def domain_condition(self, arg):
         return self.cond(arg)
 
+    def satisfy_condition(self):
+        if not self.domain_condition(self.val):
+            diff = self.val - self.dom
+            self.val = self.dom
+            # if type is <= then diff > 0, else diff < 0
+            return diff
+
 
 class RowLine:
-    def __init__(self, var, coefficients):
-        self.var = var
-        self.coef = coefficients
+    def __init__(self, coef_list):
+        self.coef = coef_list
+
+    def calc_template(self, other, op):
+        return map(op, self.coef, other)
 
     def __add__(self, other):
-        return map(lambda x, y: x + y, self.coef, other.coef)
+        return self.calc_template(other, lambda x, y: x + y)
+
+    def __sub__(self, other):
+        return self.calc_template(other, lambda x, y: x - y)
 
     def __mul__(self, const):
-        return map(lambda x: x * const, self.coef)
+        return self.calc_template(const, lambda x, y: x * y)
+
+    def __div__(self, const):
+        return self.calc_template(const, lambda x, y: x / y)
 
     def __getitem__(self, index):
         return self.coef[index]
 
 
 class Tabuleau:
-    def __init__(self, rows):
+    def __init__(self, var, rows):
+        self.var = var
         self.rows = rows
 
     def gaussian_eliminate(self):
