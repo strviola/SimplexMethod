3f4ecbcc67cdf92a7287cf0891b6bf826c5088e2
Dragon <suzuki.r.ak@m.titech.ac.jp>
2012-11-29 18:55:47 +0900

    ガウスの消去法を定義 シンプレックス法を途中まで定義

--- simplex/tabuleau.py
+++ simplex/tabuleau.py
@@ -6,9 +6,10 @@ Created on 2012/11/22
 
 class Variable:
     def __init__(self, val, slack, dom=None, dom_type=None):
-        self.val = float(val)
+        self.val = val
         self.slack = bool(slack)
         self.dom = dom
+        self.type = dom_type
 
         # define the domain condition
         if dom is None and dom_type is None:
@@ -25,47 +26,149 @@ class Variable:
         else:
             raise TypeError
 
+    def __str__(self):
+        if self.dom is None:
+            return '(%f, %s)' % (self.val, self.slack)
+        else:
+            return ('(%f %s %f, %s)' %
+                    (self.val, self.type, self.dom, self.slack))
+
     def flip_slack(self):
         self.slack = not self.slack
 
     def domain_condition(self, arg):
         return self.cond(arg)
 
+    def self_condition(self):
+        return self.cond(self.val)
+
     def satisfy_condition(self):
         if not self.domain_condition(self.val):
-            diff = self.val - self.dom
             self.val = self.dom
-            # if type is <= then diff > 0, else diff < 0
-            return diff
+
+    def get_dom_sub_val(self):
+        # if type == '>=': retval > 0 else: retval < 0
+        return self.dom - self.val
 
 
 class RowLine:
     def __init__(self, coef_list):
         self.coef = coef_list
 
-    def calc_template(self, other, op):
-        return map(op, self.coef, other)
-
-    def __add__(self, other):
-        return self.calc_template(other, lambda x, y: x + y)
+    def __str__(self):
+        return 'Row' + str(self.coef)
 
     def __sub__(self, other):
-        return self.calc_template(other, lambda x, y: x - y)
+        return RowLine(map(lambda x, y: x - y, self.coef, other))
 
     def __mul__(self, const):
-        return self.calc_template(const, lambda x, y: x * y)
+        return RowLine(map(lambda x: x * const, self.coef))
+
+    def __rmul__(self, const):
+        return self.__mul__(const)
 
     def __div__(self, const):
-        return self.calc_template(const, lambda x, y: x / y)
+        return RowLine(map(lambda x: x / const, self.coef))
 
     def __getitem__(self, index):
         return self.coef[index]
 
 
-class Tabuleau:
+class Tableau:
     def __init__(self, var, rows):
         self.var = var
         self.rows = rows
 
-    def gaussian_eliminate(self):
-        pass
+    def __str__(self):
+        info_var = 'var:['
+        for v in self.var:
+            info_var += str(v)
+            info_var += ', '
+        info_var += ']\n'
+        info_row = ''
+        for r in self.rows:
+            info_row += str(r)
+            info_row += '\n'
+        return info_var + info_row
+
+    def gaussian_elimination(self, ipivot, jpivot):
+        # divide pivot coefficient to 1
+        self.rows[ipivot] /= self.rows[ipivot][jpivot]
+        # sweep out the slack variable
+        for i, v in enumerate(self.var):
+            if v.slack:
+                self.rows[i] -= self.rows[ipivot] * self.rows[i][jpivot]
+        # change the new slack variable
+        self.rows[jpivot] -= self.rows[ipivot]
+
+    def simplex_method(self):
+        # search the slack variable that not satisfies condition
+        for i, v in enumerate(self.var):
+            if v.slack and not v.self.condition():
+                slack_i = i
+                slack_diff = v.get_dom_sub_val()
+                slack_dom = v.dom
+                break
+            return 'Satisfiable.'  # all variables satisfy condition
+        # search the non-slack variable to satisfy condition
+        for i, v in enumerate(self.var):
+            coef = self.rows[slack_i][i]  # rows expresses the coefficients
+            if not v.slack:  # find a non-slack variable
+                if slack_diff > 0:  # increase to minimum
+                    if ((coef < 0 and v.val > slack_dom) or
+                        (coef > 0 and v.val < slack_dom)):
+                        nslack_i = i
+                        break
+                elif slack_diff < 0:  # decrease to maximum
+                    if ((coef < 0 and v.val < slack_dom) or
+                        (coef > 0 and v.val > slack_dom)):
+                        nslack_i = i
+                        break
+            return 'Unsatisfiable.'  # not found the non-slack variable
+        # exchange the slack variable
+        self.var[slack_i].flip_slack()
+        self.var[nslack_i].flip_slack()
+        # update the value
+        self.var[slack_i].satisfy_condition()
+        self.var[nslack_i].val += slack_diff / coef
+        # gaussian elimination
+        
+
+if __name__ == '__main__':
+    table = Tableau([Variable(0, True), Variable(0, False),
+                     Variable(0, False), Variable(0, True),
+                     Variable(0, True)],
+                    [RowLine([0, 0, 0, 0, 0]),
+                    RowLine([0, 0, 0, 0, 0]),
+                    RowLine([1, 1, -1, 0, 0]),
+                    RowLine([2, -1, 0, -1, 0]),
+                    RowLine([-1, 2, 0, 0, -1])])
+    print table
+    table.gaussian_elimination(2, 0)
+    print table
+    # '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
