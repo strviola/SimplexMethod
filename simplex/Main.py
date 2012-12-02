a1f4902aa95b2de592d7947dc83cbac6b209c4a0
Dragon <suzuki.r.ak@m.titech.ac.jp>
2012-12-03 01:36:52 +0900

    入力画面の完成 文字列のリストとして取得

--- simplex/Main.py
+++ simplex/Main.py
@@ -6,8 +6,16 @@ Created on 2012/11/22
 
 import curses
 
+
+def bottom_alert(window, sentence):
+    win_bottom = window.getmaxyx()[0] - 1
+    window.move(win_bottom, 0)
+    window.addstr(sentence)
+
+
 if __name__ == '__main__':
     try:
+        terminate = Exception('Program terminated by quit key.')
         # start curses
         stdscr = curses.initscr()
         curses.noecho()
@@ -16,93 +24,70 @@ if __name__ == '__main__':
 
         # initialization
         stdscr.addstr(0, 0, 'Simplex Method Program v0.1 by Suzuki Ryota')
-        stdscr.addstr(1, 0, "Press 'q' to quit.")
+        stdscr.addstr(1, 0, "Press 'Q' to quit.")
+        stdscr.addstr(2, 0, "Press 'D' key to delete a character.")
         stdscr.refresh()
 
         # draw the input window
-        attr_win = stdscr.subwin(3, 0)
+        attr_win = stdscr.subwin(4, 0)
         attr_win.addstr(0, 0, 'Input the number of...')
         for_mes = 'formulas: '
         var_mes = 'variables: '
         attr_win.addstr(1, 0, for_mes)
         attr_win.addstr(2, 0, var_mes)
-        attr_win.addstr(4, 0, 'Use W, S keys as arrow keys to move.')
-        attr_win.addstr(5, 0, 'Press A key to delete a character.')
-        # remember cursor
-        for_cur_y, for_cur_x = 1, len(for_mes)
-        var_cur_y, var_cur_x = 2, len(var_mes)
-        win_bottom, win_right = attr_win.getmaxyx()
-        win_bottom -= 1
+        # cursor position
+        cur_y, cur_x = 1, len(for_mes)
+        win_bottom = attr_win.getmaxyx()[0] - 1
         # initialize input environment
-        attr_win.move(for_cur_y, for_cur_x)
+        attr_win.move(cur_y, cur_x)
         cursor_on_formula = True
-        for_buf = ''
-        var_buf = ''
+        buffers = [''] * 2
+        input_nth = 0
 
         # user input to number of formulas and variables
         while True:
+            attr_win.move(cur_y, cur_x)
             c = attr_win.getkey()
             # delete the alert message
             attr_win.move(win_bottom, 0)
             attr_win.deleteln()
             # input condition
             if c in '0123456789':  # digit
-                if cursor_on_formula:
-                    attr_win.move(for_cur_y, for_cur_x)
-                    attr_win.addstr(c)
-                    for_buf += c
-                    for_cur_x += 1
-                    attr_win.move(for_cur_y, for_cur_x)
-                else:
-                    attr_win.move(var_cur_y, var_cur_x)
-                    attr_win.addstr(c)
-                    var_buf += c
-                    var_cur_x += 1
-                    attr_win.move(var_cur_y, var_cur_x)
-            elif c == 'S' or c == 's' or c == '\n':
-                # arrow down key or enter key
-                if cursor_on_formula:  # move cursor from formula to variable
-                    cursor_on_formula = False
-                    attr_win.move(var_cur_y, var_cur_x)
+                attr_win.move(cur_y, cur_x)
+                attr_win.addstr(c)
+                buffers[input_nth] += c
+                cur_x += 1
+                attr_win.move(cur_y, cur_x)
+            elif c == '\n':  # enter key
+                if input_nth == 0:  # move cursor from formula to variable
+                    input_nth += 1
+                    cur_y, cur_x = 2, len(var_mes)
                 else:  # determine the input
-                    if (for_buf == '' or var_buf == '' or
-                        int(for_buf) == 0 or int(var_buf) == 0):
+                    if (buffers[0] == '' or buffers[1] == '' or
+                        int(buffers[0]) == 0 or int(buffers[1]) == 0):
                         # no input or number is 0
-                        attr_win.move(win_bottom, 0)
-                        attr_win.addstr('The input is not valid.')
-                        cursor_on_formula = True
-                        attr_win.move(for_cur_y, for_cur_x)
+                        bottom_alert(attr_win, 'The input is not valid.')
+                        attr_win.move(cur_y, cur_x)
                     else: break
-            elif c == 'W' or c == 'w':
-                # arrow up key: move cursor from variable to formula
-                if not cursor_on_formula:
-                    cursor_on_formula = True
-                    attr_win.move(for_cur_y, for_cur_x)
-            elif c == 'A' or c == 'a':  # arrow left key: backspace
-                if cursor_on_formula:
-                    if len(for_buf) >= 1:
-                        attr_win.delch(for_cur_y, for_cur_x - 1)
-                        for_buf = for_buf[0: len(for_buf) - 1]
-                        for_cur_x -= 1
-                        attr_win.move(for_cur_y, for_cur_x)
+            elif c == 'D' or c == 'd':  # arrow left key: backspace
+                if len(buffers[input_nth]) >= 1:
+                    attr_win.delch(cur_y, cur_x - 1)
+                    buffers[input_nth] = buffers[input_nth][: -1]
+                    cur_x -= 1
+                    attr_win.move(cur_y, cur_x)
                 else:
-                    if len(for_buf) >= 1:
-                        attr_win.delch(var_cur_y, var_cur_x - 1)
-                        var_buf = var_buf[0: len(var_buf) - 1]
-                        var_cur_x -= 1
-                        attr_win.move(var_cur_y, var_cur_x)
+                    bottom_alert(attr_win, 'The input is already empty.')
             elif c == 'q':  # quit
-                raise Exception('Exit by quit key.')
+                raise terminate
             else:
-                attr_win.move(win_bottom, 0)
-                attr_win.addstr('The input %s is not a digit.' % c)
+                bottom_alert(attr_win, 'The input %s is not a digit.' % c)
 
         # result of each numbers
-        for_num = int(for_buf)
-        var_num = int(var_buf)
+        for_num = int(buffers[0])
+        var_num = int(buffers[1])
         # change the window
         attr_win.erase()
-        form_win = stdscr.subwin(3, 0)
+        form_win = stdscr.subwin(4, 0)
         form_win.addstr(0, 0, 'Input %d formulas with %d variables.' %
                         (for_num, var_num))
         usage_text = 'For example: '
@@ -110,10 +95,81 @@ if __name__ == '__main__':
             usage_text += '%d ' % i
         usage_text += '>= %d' % var_num
         form_win.addstr(for_num + 2, 0, usage_text)
+        # cursor usage
+        cur_y = 1
+        cur_x = 0
+        form_win.move(cur_y, cur_x)
+        first_flag = True
+        win_bottom = form_win.getmaxyx()[0] - 1
+        # remember for max input number
+        for_nth = 0  # <= for_num
+        var_nth = 0  # var_nth[n] <= var_num
+        # buffers
+        buffers = [''] * for_num
         # user input to formulas
         while True:
+            form_win.move(cur_y, cur_x)
             c = form_win.getkey()
-            if c == 'q': break
+            # delete the alert message
+            form_win.move(win_bottom, 0)
+            form_win.deleteln()
+            form_win.move(cur_y, cur_x)
+            if c in '0123456789':
+                if var_nth <= var_num - 1 or var_nth == var_num + 1:
+                    buffers[for_nth] += c
+                    form_win.addstr(c)
+                    cur_x += 1
+                    first_flag = False
+                else:
+                    bottom_alert(form_win, 'Input a sign of inequality')
+            elif c == '-':
+                if first_flag:
+                    form_win.addstr(c)
+                    buffers[for_nth] += c
+                    cur_x += 1
+                    first_flag = False
+                else:
+                    bottom_alert(form_win, 'You cannot input %s to here.' % c)
+            elif c == ' ':
+                if var_nth <= var_num - 1 and not first_flag:
+                    form_win.addstr(c)
+                    buffers[for_nth] += c
+                    var_nth += 1
+                    cur_x += 1
+                    first_flag = True
+                else:
+                    bottom_alert(form_win, 'You cannot input space here.')
+            elif (c == '<' or c == '>') and first_flag:
+                if var_nth == var_num:
+                    form_win.addstr(c + "=")
+                    buffers[for_nth] += c + '= '
+                    cur_x += 3
+                    var_nth += 1
+                    first_flag = False
+                else:
+                    bottom_alert(form_win, 'You cannot input %s here.' % c)
+            elif c == '\n':  # move to next formula
+                if for_nth <= for_num - 2:
+                    var_nth = 0
+                    cur_y += 1
+                    cur_x = 0
+                    for_nth += 1
+                else:
+                    break
+            elif c == 'd' or c == 'D':
+                if len(buffers[for_nth]) >= 1:
+                    form_win.delch(cur_y, cur_x - 1)
+                    if buffers[-1] == ' ':  # last token is space
+                        var_nth -= 1
+                        first_flag = True
+                    buffers[for_nth] = buffers[for_nth][: -1]
+                    cur_x -= 1
+                else:
+                    bottom_alert(form_win, 'The input is already empty.')
+            elif c == 'q':
+                raise terminate
+            else:
+                bottom_alert(form_win, 'You cannot input %s to here.' % c)
 
         exit_mes = 'Program terminated successfully.'
 
@@ -121,8 +177,11 @@ if __name__ == '__main__':
         exit_mes = '%s, %s' % (type(e), e)
 
     finally:
+        # return to normal window
         curses.nocbreak()
         stdscr.keypad(0)
         curses.echo()
         curses.endwin()
-        print exit_mes
+
+        # convert the input to simplex tableau
+        pass
